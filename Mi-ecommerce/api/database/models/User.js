d9b7d7036d4aa131da7d33ca6171143cfcb90bc3
cabrerajuanjo <juanjosecabrera@G500606MAC346.local>
2022-09-22 17:34:47 -0300

    Se crea modelo User

---
+++ Mi-ecommerce/api/database/models/User.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = 'User';
+    const cols = {
+        user_id: {
+            type: DataTypes.BIGINT,
+			primaryKey: true,
+			autoIncrement: true,
+			allowNull: false
+        },
+        first_name: { 
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: { 
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: { 
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: { 
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        role: { 
+            type: DataTypes.ENUM('admin' , 'god', 'guest'),
+            defaultValue: 'guest',
+            allowNull: true
+        },
+        profilepic: { 
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    }
+
+    const config = {
+        timestamps: false
+    }
+
+    const User = sequelize.define(alias, cols, config);
+    
+    return User;
+}
