38699fec5dce086d90177587755788694b3e2424
juanch013 <felipeparada99@hotmail.com>
2022-09-23 17:22:53 -0300

    elimino comentarios inutiles

--- Mi-ecommerce/api/controllers/productsController.js
+++ Mi-ecommerce/api/controllers/productsController.js
@@ -24,8 +24,6 @@ const productsController = {
                 ]
             })
 
-            // products = products.filter((prod)=>{return (prod.category).toLowerCase() == (category).toLowerCase()});
-
             if(products.length == 0){
                 return res.status(404).json({
                     error:true,
@@ -33,9 +31,6 @@ const productsController = {
                 })
             }
             
-            // for(prod of products){
-            //          prod.gallery = fileHelpers.getPicturesFromProduct(prod.id,next);
-            // }
 
             return res.status(200).json({
                 error: false,
@@ -53,9 +48,6 @@ const productsController = {
                     }
                 ]
             });
-            // for(prod of products){
-            //     prod.gallery = fileHelpers.getPicturesFromProduct(prod.id,res,next);
-            // }
     
             return res.status(200).json({
                         error:false,
@@ -93,34 +85,10 @@ const productsController = {
                         msg: "Product detail",
                         data: prod
                     });
-
-        // for(prod of products){
-        //     if(prod.id == id){
-        //         prod.gallery = fileHelpers.getPicturesFromProduct(prod.id,next);
-        //         return res.status(200).json({
-        //             error: false,
-        //             msg: "Product detail",
-        //             data: prod
-        //         })
-        //     }
-        // }
-
         
     },
 
     mostwanted: async (req, res, next)=>{
-        // let products = fileHelpers.getProducts(next);
-
-
-        // let filteredProducts = products.filter((prod)=>{
-        //     return prod.mostwanted == true;
-        // })
-
-
-
-        // for(prod of filteredProducts){
-        //     prod.gallery = fileHelpers.getPicturesFromProduct(prod.id,next);
-        // }
 
         let products = await db.Product.findAll({
             where:{
@@ -164,27 +132,6 @@ const productsController = {
 
         db.Product.create(newProduct);
 
-
-        // let products = fileHelpers.getProducts(next);
-        // products = fileHelpers.ordenarProductos(products);
-
-        // let newId = Number(products[products.length - 1].id) + 1;
-
-        // const newProduct = {
-        //     id: newId,
-        //     price: price,
-        //     title: title,
-        //     description : description == undefined? "" : description,
-        //     gallery: [],
-        //     stock: stock == undefined? 0 : stock,
-        //     mostwanted : mostwanted == undefined? false : mostwanted,
-        //     category : category
-        // }
-
-        // products.push(newProduct);
-
-        // fileHelpers.guardarProducts(products,next);
-
          return res.status(201).json({
              error:false,
              msg:"Product created",
@@ -222,51 +169,11 @@ const productsController = {
                     error: false,
                     msg:"Product deleted"
                 })
-
-        // let products = fileHelpers.getProducts(next);
-
-        // if(!products.some((p)=>{ return p.id == id})){
-        //     return res.status(404).json({
-        //         error: true,
-        //         msg:"Product not found"
-        //     })
-        // }
-
-
-        
-        // //elimina las imagenes del product que se va a eliminar
-        // fileHelpers.eliminarPicturesDeProduct(id,next);
-        
-
-        // //filtra os productos y guarda el array que no contiene el producto con producto.id == 'id'
-        // products = products.filter((prod)=>{return prod.id != id});
-        // fileHelpers.guardarProducts(products ,next);
     },
 
     busqueda: async (req, res, next)=>{
-        // let products = fileHelpers.getProducts(next);
         const {q} = req.query; 
         
-        //aca recorro el array de productos y voy pusheando a 'productsFiltrados' los elementos
-        //que contengan la keyword del query string
-
-        // products.forEach(element => {
-        //     if(element.description.toLowerCase().includes(q.toLowerCase()) || element.title.toLowerCase().includes(q.toLowerCase
-        //       ())){
-        //         productsFiltrados.push(element);
-        //     } 
-        // });
-
-        // for(prod of productsFiltrados){
-        //     prod.gallery = fileHelpers.getPicturesFromProduct(prod.id,next);
-        // }
-
-        // if (productsFiltrados.length == 0){
-        //     return res.status(404).json({
-        //         error: true,
-        //         msg: "No products found"
-        //     })
-        // }
 
         console.log(q);
 
@@ -337,42 +244,7 @@ const productsController = {
                 product_id : id
             }
         })
-
-
-        // let n = db.Product.update({
-
-        // },{
-        //     where:{
-
-        //     }
-        // })
-
-        // let products = fileHelpers.getProducts(next);
-
-        // if(!products.some((prod)=>{return prod.id == id})){
-        //     return res.status(404).json({
-        //         msg:`Doesn't exist a product with id ${id}`
-        //     })
-        // }
-
-        // let prodModificado = {};
-
-        // for(prod of products){
-        //     if(prod.id == id){
-        //         prod.title = title == undefined || title == "" ? prod.title : title;
-        //         prod.description = description == undefined || description == "" ? prod.description : description;
-        //         prod.price = price == undefined? prod.price : price;
-        //         prod.stock = stock == undefined? prod.stock : stock;
-        //         prod.mostwanted = mostwanted == undefined? prod.mostwanted : mostwanted;
-        //         prod.category = category == undefined? prod.undefined : undefined;
-        //         prod.gallery = gallery == undefined? prod.gallery : gallery;
-        //         prodModificado = prod;
-        //         break;
-        //     }
-        // }
         
-        // fileHelpers.guardarProducts(products, next);
-
         return res.status(200).json({
             error: false,
             msg:"Product modified",
@@ -415,48 +287,7 @@ const productsController = {
         } catch (error) {
             next(error);
         }
-    //   try {
-    //     const { id } = req.params;
-
-    //     if (
-    //       req.newUsers.role !== 'admin' &&
-    //       req.newUsers.role !== 'guest' &&
-    //       req.newUsers.role !== 'god'
-    //     ) {
-    //       return res.status(401).json({ 
-    //         error: true,
-    //         msg: 'You are not authorized to access this resource',
-    //       });
-    //     }
-    
-    //     const products = fileHelpers.getProducts(res, next);
-    
-    //     const productExists = products.find(
-    //       (product) => product.id === parseInt(id)
-    //     );
-    //     if (!productExists) {
-    //       return res.status(404).json({ error: 'Product not found', message: '' });
-    //     }
-    
-    //     // Se lee el arhivo de pictures
-    //     const pictures = fileHelpers.getImages(next);
-    
-    //     const picturesProduct = pictures?.filter(
-    //       (picture) => picture.productId === parseInt(id)
-    //     );
-    
-    //     if (!picturesProduct.length) {
-    //       return res
-    //         .status(404)
-    //         .json({ error: 'The product does not have images', message: '' });
-    //     }
-    
-    //     res.status(200).json(picturesProduct);
-    //   } catch (error) {
-    //     next(error);
-    //   }
     },
-//------------------- hasta aca actualizado a sequelize
     categoria: async (req, res, next)=>{
 
         const {category} = req.query;
