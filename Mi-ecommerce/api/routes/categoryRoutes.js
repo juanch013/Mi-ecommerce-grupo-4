5f921160d7efcac73b73fca0eb2e6e135f773d3d
Mokubag <juan.galvan@fing.edu.uy>
2022-09-23 19:16:10 -0300

    agregue validaciones de usuario a las categorias

--- Mi-ecommerce/api/routes/categoryRoutes.js
+++ Mi-ecommerce/api/routes/categoryRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const { idByParamsSchema } = require('../schemas/genericSchema');
 const categoryController = require('../controllers/categoryControler');
 const validatorHandler = require('../middlewares/validatorHandler');
-	
-router.get('/',categoryController.listCategory);
-router.post('/',categoryController.createCategory);
-router.delete('/:id',validatorHandler(idByParamsSchema, 'params'),categoryController.deleteCategory);
-router.put('/:id',validatorHandler(idByParamsSchema, 'params'),categoryController.updateCategory);
+const rolVerification = require('../middlewares/rolVerification');	
+const { verifyJWT } = require('../middlewares/verifyJWT');
+router.use(verifyJWT);
+
+router.get('/',rolVerification.generic,categoryController.listCategory);
+router.post('/',rolVerification.admin,categoryController.createCategory);
+router.delete('/:id',rolVerification.admin,validatorHandler(idByParamsSchema, 'params'),categoryController.deleteCategory);
+router.put('/:id',rolVerification.admin,validatorHandler(idByParamsSchema, 'params'),categoryController.updateCategory);
 module.exports = router;
