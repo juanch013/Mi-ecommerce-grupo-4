fa2757dc1079384ecdf1f89a45586d9ef62d7360
Mokubag <juan.galvan@fing.edu.uy>
2022-09-26 15:33:18 -0300

    carga de datos update

--- Mi-ecommerce/api/controllers/cargarDatosControler.js
+++ Mi-ecommerce/api/controllers/cargarDatosControler.js
@@ -1,10 +1,13 @@
 const db = require('../database/models');
 // Carga de jsons
 const users = require('../../api/data.json/user.json');
+const products = require('../../api/data.json/products.json');
+const categories = require('../../api/data.json/categories.json');
 const cargarDatosController = {
     carga: async function(req,res){
+        //cargar usuarios
         for await (let u of users) {
-            await db.User.create({
+            await db.User.upsert({
                 first_name: u.first_name,
                 last_name: u.last_name,
                 username: u.username,
@@ -17,7 +20,6 @@ const cargarDatosController = {
         res.status(200).json({
             error:false,
             msg: "Se cargaron los datos exitosamente",
-            data:newcategory
         });
     }
 }
