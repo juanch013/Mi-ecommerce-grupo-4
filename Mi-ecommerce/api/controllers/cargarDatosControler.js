02656f66fd6528eb90f109863c2257d99de1d8d2
Mokubag <juan.galvan@fing.edu.uy>
2022-09-26 16:29:18 -0300

    git no amigable

--- Mi-ecommerce/api/controllers/cargarDatosControler.js
+++ Mi-ecommerce/api/controllers/cargarDatosControler.js
@@ -17,6 +17,35 @@ const cargarDatosController = {
                 profilpic: u.profilepic,
             });
         }
+            //devolver un array de los id de usuarios creados
+    const users2 = await db.User.findAll({
+        attributes: ['user_id']
+      })
+      const usersMapped = users2.map(user => user.user_id)
+    
+      //crear por cada id de usuario un carrito 
+      for await (let id of usersMapped) {
+        await db.Cart.upsert({
+          user_id: id,
+          cart_id: id
+        })
+      }
+  
+      for await (let category of categories) {
+        await db.Category.upsert({
+          category_name: category.category_name
+        })
+      }
+  
+      for await (let product of products) {
+        await db.Product.upsert({
+          price: product.price,
+          title: product.title,
+          stock: product.stock,
+          mostwanted: product.mostwanted,
+          category: product.category
+        })
+      }
         res.status(200).json({
             error:false,
             msg: "Se cargaron los datos exitosamente",
